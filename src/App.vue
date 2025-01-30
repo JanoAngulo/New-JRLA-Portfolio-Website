9173204cf7f72c332a1d070796a5e7b779fdfe33
JanoAngulo <93524736+JanoAngulo@users.noreply.github.com>
2025-01-30 22:06:03 +0800

    added hidden console message

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,30 @@
 </template>
 
 <script setup>
-import Home from './components/Home.vue'
-import Navbar from './components/Navbar.vue'
-import Features from './components/Features.vue'
-import Portfolio from './components/Portfolio.vue'
-import Resume from './components/Resume.vue'
-import Contact from './components/Contact.vue'
+  import Home from './components/Home.vue'
+  import Navbar from './components/Navbar.vue'
+  import Features from './components/Features.vue'
+  import Portfolio from './components/Portfolio.vue'
+  import Resume from './components/Resume.vue'
+  import Contact from './components/Contact.vue'
+
+  import { onMounted } from 'vue'
+
+  onMounted(() => {
+    console.log(`
+   ##   #####    #          #
+   ##   #    #   #         # #
+   ##   #####    #        #####
+   ##   #  ##    #       #     #
+###    #    ##   #####  #       #
+
+Hello there! 👋
+It seems like you are a curious one! 🧐
+But it's ok, curiosity is the first step towards knowledge. 🧠
+Feel free to explore my portfolio and learn more about me. 🚀
+
+If you have any questions or just want to say hi, don't hesitate to reach out to using my portfolio contact form or directly at my email: Angulo.johnrussel.l.7814@gmail.com 📧
+
+Have a great day! 🌞`)
+  })
 </script>
